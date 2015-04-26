9278624394feceda3f8fc5f334b55ebfba5e1e2f
Evan Lynch <evan.f.lynch@gmail.com>
2015-04-26 19:50:33 -0400

    Pushing refactor

---
+++ display.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+from kivy.logger import Logger
+from kivy.graphics import PushMatrix, PopMatrix, RenderContext, Callback
+from kivy.graphics import UpdateNormalMatrix, Translate, Rotate, Mesh
+from kivy.graphics.transformation import Matrix
+import kivy.graphics.opengl as gl
+
+from kivy.clock import Clock as kivyClock
+from kivy.resources import resource_find
+
+from eran.core import run, BaseWidget
+from objloader import ObjFileLoader
+import synth
+
+
+class DisplayController(object):
+    def __init__(self, width, height, canvas):
+        super(DisplayController, self).__init__()
+        self.canvas = canvas
+        self.width = width
+        self.height = height
+        self.canvas.shader.source = resource_find('simple.glsl')
+        self.scene = ObjFileLoader(resource_find("testnurbs.obj"))
+        with self.canvas:
+            self.cb = Callback(self.setup_gl_context)
+            PushMatrix()
+            self.setup_scene()
+            PopMatrix()
+
+    def setup_gl_context(self, *args):
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
+    def reset_gl_context(self, *args):
+        gl.glDisable(gl.GL_DEPTH_TEST)
+
+    def get_look_at(self, x, y, z, azi, ele):
+        dx = - np.sin(azi) * np.cos(ele)
+        dy = np.sin(ele)
+        dz = - np.cos(azi) * np.cos(ele)
+
+        # Not sure why up has to just be up...
+        upx = 0
+        upy = 1
+        upz = 0
+
+        mat = Matrix()
+        mat = mat.look_at(x, y, z,
+                          x + dx, y + dy, z + dz,
+                          upx, upy, upz)
+        return mat
+
+    def update_camera(self, x, y, z, azi, ele):
+        asp = self.width / float(self.height)
+        mat = self.get_look_at(x, y, z, azi, ele)
+
+        proj = Matrix()
+        proj.perspective(30, asp, 1, 50)
+
+        self.canvas['projection_mat'] = proj
+        self.canvas['modelview_mat'] = mat
+
+    def setup_scene(self):
+        PushMatrix()
+        UpdateNormalMatrix()
+        self.draw_elements()
+        PopMatrix()
+
+    def draw_elements(self):
+        """ Draw separately all objects on the scene
+            to setup separate rotation for each object
+        """
+        def _draw_element(m):
+            Mesh(
+                vertices=m.vertices,
+                indices=m.indices,
+                fmt=m.vertex_format,
+                mode='triangles',
+            )
+        # Draw sphere in the center
+        sphere = self.scene.objects['Sphere']
+        self.sphere_trans = Translate(0, 0, -10)
+        _draw_element(sphere)
+
+        sphere2 = self.scene.objects['Sphere']
+        self.sphere2_trans = Translate(0, 0, 10)
+        _draw_element(sphere2)
--- main.py
+++ main.py
@@ -4,204 +4,118 @@ LOG_LEVEL = 'info'
 
 from kivy.config import Config
 Config.set('kivy', 'log_level', LOG_LEVEL)
-
-from kivy.graphics import PushMatrix, PopMatrix, RenderContext, Callback
-from kivy.graphics import UpdateNormalMatrix, Translate, Rotate, Mesh
-from kivy.graphics.transformation import Matrix
-import kivy.graphics.opengl as gl
-
+from kivy.graphics import RenderContext
 from kivy.clock import Clock as kivyClock
 from kivy.resources import resource_find
 
 from eran.core import run, BaseWidget
 from objloader import ObjFileLoader
+from display import DisplayController
 import synth
 
 
 class MainWidget(BaseWidget):
-	def __init__(self):
-		super(MainWidget, self).__init__()
-		self.canvas = RenderContext(compute_normal_mat=True)
-		self.canvas.shader.source = resource_find('simple.glsl')
-		self.scene = ObjFileLoader(resource_find("testnurbs.obj"))
-		with self.canvas:
-			self.cb = Callback(self.setup_gl_context)
-			PushMatrix()
-			self.setup_scene()
-			PopMatrix()
-			self.cb = Callback(self.reset_gl_context)
-			kivyClock.schedule_interval(self.update_scene, 1 / 60.)
-		self._touches = []
-		self.eyex = 0.
-		self.eyey = 0.
-		self.eyez = 0.
-		self.eye_azimuth = 0.
-		self.eye_elevation = 0.
-		assert(np.pi / 2 >= self.eye_azimuth >= - np.pi / 2)
-		assert(np.pi > self.eye_azimuth >= -np.pi)
+    def __init__(self):
+        super(MainWidget, self).__init__()
+        self.canvas = RenderContext(compute_normal_mat=True)
+        self.display = DisplayController(self.width, self.height, self.canvas)
+        self.canvas = self.display.canvas
+        self._touches = []
+        self.eye_x = 0.
+        self.eye_y = 0.
+        self.eye_z = 0.
+        self.eye_azimuth = 0.
+        self.eye_elevation = 0.
 
+    def update_camera(self):
+        self.display.update_camera(self.eye_x, self.eye_y, self.eye_z,
+                                   self.eye_azimuth, self.eye_elevation)
 
-	def setup_gl_context(self, *args):
-		gl.glEnable(gl.GL_DEPTH_TEST)
+    def set_camera_pos(self, x, y, z):
+        self.eye_x = x
+        self.eye_y = y
+        self.eye_z = z
+        self.update_camera()
+        synth.send_camera_pos(self.eye_x, self.eye_y, self.eye_z)
 
-	def reset_gl_context(self, *args):
-		gl.glDisable(gl.GL_DEPTH_TEST)
+    def move_camera_pos(self, dx, dy, dz):
+        self.set_camera_pos(self.eye_x + dx, self.eye_y + dy, self.eye_z + dz)
 
-	def get_look_at(self):
-		dx = - np.sin(self.eye_azimuth) * np.cos(self.eye_elevation)
-		dy = np.sin(self.eye_elevation)
-		dz = - np.cos(self.eye_azimuth) * np.cos(self.eye_elevation)
+    def set_camera_angle(self, azi, ele):
+        self.eye_azimuth = azi
+        self.eye_elevation = ele
+        self.rectify_angles()
+        self.update_camera()
+        synth.send_camera_angle(self.eye_azimuth * 180 / np.pi,
+                                self.eye_elevation * 180 / np.pi)
 
-		# Not sure why up has to just be up...
-		upx = 0
-		upy = 1
-		upz = 0
+    def move_camera_angle(self, dazi, dele):
+        self.set_camera_angle(self.eye_azimuth + dazi, self.eye_elevation + dele)
 
-		mat = Matrix()
-		mat = mat.look_at(self.eyex, self.eyey, self.eyez,
-						  self.eyex + dx, self.eyey + dy, self.eyez + dz,
-						  upx, upy, upz)
-		return mat
+    def on_key_down(self, keycode, modifiers):
+        SPEED = 0.2
+        dy = dx = dz = dazi = dele = 0
+        if keycode[1] == 'q': # up
+            dy = -SPEED
+        elif keycode[1] == 'e': # down
+            dy = SPEED
+        elif keycode[1] == 'd': # left strafe
+            dz = - SPEED * np.sin(self.eye_azimuth)
+            dx = SPEED * np.cos(self.eye_azimuth)
+        elif keycode[1] == 'a': # right strafe
+            dz = SPEED * np.sin(self.eye_azimuth)
+            dx = - SPEED * np.cos(self.eye_azimuth)
+        elif keycode[1] == 's': # backwards
+            dz = SPEED * np.cos(self.eye_azimuth)
+            dx = SPEED * np.sin(self.eye_azimuth)
+        elif keycode[1] == 'w': # forwards
+            dz = - SPEED * np.cos(self.eye_azimuth)
+            dx = - SPEED * np.sin(self.eye_azimuth)
 
-	def update_glsl(self, *largs):
-		asp = self.width / float(self.height)
-		mat = self.get_look_at()
+        elif keycode[1] == 'z':
+            dazi = SPEED / 4
+        elif keycode[1] == 'x':
+            dazi = - SPEED / 4
 
-		proj = Matrix()
-		proj.perspective(30, asp, 1, 50)
+        elif keycode[1] == 'v':
+            dele = SPEED / 4
+        elif keycode[1] == 'c':
+            dele = - SPEED / 4
 
-		self.canvas['projection_mat'] = proj
-		self.canvas['modelview_mat'] = mat
+        self.move_camera_angle(dazi, dele)
+        self.move_camera_pos(dx, dy, dz)
 
-	def setup_scene(self):
-		PushMatrix()
-		UpdateNormalMatrix()
-		self.draw_elements()
-		PopMatrix()
+    def define_rotate_angle(self, touch):
+        x_angle = - (touch.dx/self.width) * 2 * np.pi
+        y_angle = (touch.dy/self.height) * np.pi
+        return x_angle, y_angle
 
-	def draw_elements(self):
-		""" Draw separately all objects on the scene
-			to setup separate rotation for each object
-		"""
-		def _draw_element(m):
-			Mesh(
-				vertices=m.vertices,
-				indices=m.indices,
-				fmt=m.vertex_format,
-				mode='triangles',
-			)
-		# Draw sphere in the center
-		sphere = self.scene.objects['Sphere']
-		self.sphere_trans = Translate(0, 0, -10)
-		_draw_element(sphere)
+    def rectify_angles(self):
+        if self.eye_azimuth >= np.pi:
+            self.eye_azimuth -= 2 * np.pi
+        elif self.eye_azimuth < - np.pi:
+            self.eye_azimuth += 2 * np.pi
 
-		sphere2 = self.scene.objects['Sphere']
-		self.sphere2_trans = Translate(0, 0, 10)
-		_draw_element(sphere2)
+        if self.eye_elevation > np.pi / 2.0 :
+            self.eye_elevation = np.pi / 2.0
+        elif self. eye_elevation < - np.pi / 2.0:
+            self.eye_elevation = - np.pi / 2.0
 
-	def update_scene(self, *largs):
-		self.update_glsl()
-		# TODO: make the balls move
+    def on_touch_down(self, touch):
+        # print touch.profile.
+        self._touch = touch
+        touch.grab(self)
+        self._touches.append(touch)
 
-	def set_camera_pos(self, x, y, z):
-		self.eyex = x
-		self.eyey = y
-		self.eyez = z
-		synth.send_camera_pos(self.eyex, self.eyey, self.eyez)
+    def on_touch_up(self, touch): 
+        touch.ungrab(self)
+        self._touches.remove(touch)
 
-	def move_camera_pos(self, dx, dy, dz):
-		self.eyex += dx
-		self.eyey += dy
-		self.eyez += dz
-		synth.send_camera_pos(self.eyex, self.eyey, self.eyez)
+    def on_touch_move(self, touch):
+        if touch in self._touches and touch.grab_current == self:
+            if len(self._touches) == 1:
+                dazi, dele = self.define_rotate_angle(touch)
+                self.move_camera_angle(dazi, dele)
 
-	def set_camera_angle(self, azi, ele):
-		self.eye_azimuth = azi
-		self.eye_elevation = ele
-		self.rectify_angles()
-		synth.send_camera_angle(self.eye_azimuth * 180 / np.pi, self.eye_elevation * 180 / np.pi)
-
-	def move_camera_angle(self, dazi, dele):
-		self.eye_azimuth += dazi
-		self.eye_elevation += dele
-		self.rectify_angles()
-		synth.send_camera_angle(self.eye_azimuth * 180 / np.pi, self.eye_elevation * 180 / np.pi)
-
-	def on_key_down(self, keycode, modifiers):
-		SPEED = 0.2
-		dy = dx = dz = dazi = dele = 0
-		if keycode[1] == 'q': # up
-			dy = -SPEED
-		elif keycode[1] == 'e': # down
-			dy = SPEED
-		elif keycode[1] == 'd': # left strafe
-			dz = - SPEED * np.sin(self.eye_azimuth)
-			dx = SPEED * np.cos(self.eye_azimuth)
-		elif keycode[1] == 'a': # right strafe
-			dz = SPEED * np.sin(self.eye_azimuth)
-			dx = - SPEED * np.cos(self.eye_azimuth)
-		elif keycode[1] == 's': # backwards
-			dz = SPEED * np.cos(self.eye_azimuth)
-			dx = SPEED * np.sin(self.eye_azimuth)
-		elif keycode[1] == 'w': # forwards
-			dz = - SPEED * np.cos(self.eye_azimuth)
-			dx = - SPEED * np.sin(self.eye_azimuth)
-
-		elif keycode[1] == 'z':
-			dazi = SPEED / 4
-		elif keycode[1] == 'x':
-			dazi = - SPEED / 4
-
-		elif keycode[1] == 'v':
-			dele = SPEED / 4
-		elif keycode[1] == 'c':
-			dele = - SPEED / 4
-
-		self.move_camera_angle(dazi, dele)
-		self.move_camera_pos(dx, dy, dz)
-
-	def define_rotate_angle(self, touch):
-		x_angle = - (touch.dx/self.width) * 2 * np.pi
-		y_angle = (touch.dy/self.height) * np.pi
-		return x_angle, y_angle
-
-	def rectify_angles(self):
-		if self.eye_azimuth >= np.pi:
-			self.eye_azimuth -= 2 * np.pi
-		elif self.eye_azimuth < - np.pi:
-			self.eye_azimuth += 2 * np.pi
-
-		if self.eye_elevation > np.pi / 2.0 :
-			self.eye_elevation = np.pi / 2.0
-		elif self. eye_elevation < - np.pi / 2.0:
-			self.eye_elevation = - np.pi / 2.0
-
-	def on_touch_down(self, touch):
-		# print touch.profile.
-		self._touch = touch
-		touch.grab(self)
-		self._touches.append(touch)
-
-	def on_touch_up(self, touch): 
-		touch.ungrab(self)
-		self._touches.remove(touch)
-
-	def on_touch_move(self, touch):
-		self.update_glsl()
-
-		if touch in self._touches and touch.grab_current == self:
-			if len(self._touches) == 1:
-				dazi, dele = self.define_rotate_angle(touch)
-				self.move_camera_angle(dazi, dele)
-
-
-class AudioController(object):
-	def __init__(self):
-		super(AudioController, self).__init__()
-
-
-class DisplayController(object):
-	def __init__(self):
-		super(DisplayController, self).__init__()
 
 run(MainWidget)
