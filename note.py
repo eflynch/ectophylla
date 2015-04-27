b448cd89c86e1fbaa1512c1cb40bcfe61e433498
Emily <emzhang@mit.edu>
2015-04-27 00:33:37 -0400

    note details

--- note.py
+++ note.py
@@ -1,12 +1,8 @@
 class Note(object):
-	def __init__(self, pitch, velocity, duration, start_pos, end_pos, time, x, y, tp, tick):
+	def __init__(self, pitch, velocity, duration, x, y, tp):
 		self.pitch = pitch
 		self.velocity = velocity
 		self.duration = duration
-		self.start_pos = start_pos
-		self.end_pos = end_pos
-		self.time = time
 		self.x = x
 		self.y = y
 		self.tp = tp
-		self.tick = tick
--- score_parser.py
+++ score_parser.py
@@ -4,13 +4,12 @@ def parse(path):
 	all_notes = []
 	score_file = open(path)
 	for line in score_file:
-		pitch, velocity, duration, time, x, y, tp = line.rstrip().split(" ")
+		pitch, velocity, duration, x, y, tp = line.rstrip().split(" ")
 		pitch = float(pitch)
 		velocity = float(velocity)
 		duration = int(duration)
-		time = tuple(time)
 		x = float(x)
 		y = float(y)
 		tp = float(tp)
-		all_notes.append(Note(pitch, velocity, duration, time, x, y, tp))
+		all_notes.append(Note(pitch, velocity, duration, x, y, tp))
 	return all_notes
