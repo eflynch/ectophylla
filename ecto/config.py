06610d32ab2b27aa337623be259be99e29362218
Evan Lynch <evan.f.lynch@gmail.com>
2015-05-04 11:30:21 -0400

    Who knows...

--- ecto/config.py
+++ ecto/config.py
@@ -1,9 +1,9 @@
 config = {
     'PLANE_SPACING': 50,
     'LINE_SPACING': 5,
-    'UNITS_PER_METER': 2.0,
+    'UNITS_PER_METER': 2.5,
     'UNITS_PER_TICK': 30./480.,
-    'SELF_PLANE_DISTANCE': 5,
+    'SELF_PLANE_DISTANCE': 8,
     'OSC_OUT_PORT': 5555,
     'VISIBLE_TICK_RANGE': 480 * 5
 }
